127480dbdd19a32645ea4fe40f0a1876f97976a4
Felix Lange <fjl@twurst.com>
2015-07-28 14:06:44 +0200

    upgrade packages

--- elpa/eshell-z-20150724.55/eshell-z-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; eshell-z-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("eshell-z-pkg.el" "eshell-z.el") (21938
-;;;;;;  20989 227830 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; eshell-z-autoloads.el ends here
---
+++ elpa/eshell-z-20150725.2027/eshell-z-autoloads.el
@@ -0,0 +1,15 @@
+;;; eshell-z-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("eshell-z.el") (21943 28707 860440 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eshell-z-autoloads.el ends here
