76c533504a4f17a11781e022c7b2ecb3bd46ddf8
Felix Lange <fjl@twurst.com>
2015-08-19 02:21:25 +0200

    add go-scratch

---
+++ elpa/go-scratch-20150809.2140/go-scratch-autoloads.el
@@ -0,0 +1,22 @@
+;;; go-scratch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "go-scratch" "go-scratch.el" (21971 32709 0
+;;;;;;  0))
+;;; Generated autoloads from go-scratch.el
+
+(autoload 'go-scratch "go-scratch" "\
+Go to the Go scratch buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; go-scratch-autoloads.el ends here
