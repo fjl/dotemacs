a3287b511cb8a61eddb1c1af1266cf1927475a2e
Felix Lange <fjl@twurst.com>
2015-09-30 16:30:39 +0200

    autoremove dependency packages

--- elpa/faceup-20150215.1348/faceup-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "faceup" "20150215.1348" "Regression test system for font-lock" 'nil :url "https://github.com/Lindydancer/faceup" :keywords '("faces" "languages"))
--- elpa/shorten-20150306.517/shorten-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "shorten" "20150306.517" "component-wise string shortener" 'nil :url "https://github.com/jorgenschaefer/circe/blob/master/lisp/shorten.el" :keywords '("extensions"))
