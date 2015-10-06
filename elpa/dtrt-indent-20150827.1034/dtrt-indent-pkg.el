b1ba2dfaf09cae58f7b65bfc7ffcb58339841f66
Felix Lange <fjl@twurst.com>
2015-10-06 23:30:19 +0200

    upgrade packages

--- elpa/dtrt-indent-20150827.1034/dtrt-indent-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "dtrt-indent" "20150827.1034" "Adapt to foreign indentation offsets" 'nil :keywords
-  '("convenience" "files" "languages" "c"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/eshell-z-20150920.258/eshell-z-pkg.el
@@ -0,0 +1,1 @@
+(define-package "eshell-z" "20150920.258" "cd to frequent directory in eshell" '((cl-lib "0.5")) :url "https://github.com/xuchunyang/eshell-z" :keywords '("convenience"))
