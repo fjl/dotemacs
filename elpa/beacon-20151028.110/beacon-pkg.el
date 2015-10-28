4db2551ba700af5dd8c2e131f91b695520eef0f6
Felix Lange <fjl@twurst.com>
2015-10-28 19:41:32 +0100

    remove beacon (too slow)

--- elpa/beacon-20151028.110/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20151028.110" "Highlight the cursor whenever the window scrolls" '((seq "1.9")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
