57c9ca06b449f33cf3887d2fd31b63740d533d12
Felix Lange <fjl@twurst.com>
2015-12-07 18:08:32 +0100

    fix magit-visit-pull-request-url

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -7,16 +7,16 @@
 The branch must be pushed to github and have a remote."
   (interactive)
   (let* ((branch      (magit-get-current-branch))
-         (base-info   (fjl/get-github-info *pr-base-branch*))
-         (branch-info (fjl/get-github-info branch)))
+         (base-info   (fjl/get-github-info *pr-base-branch* #'magit-get-remote))
+         (branch-info (fjl/get-github-info branch #'magit-get-push-remote)))
     (browse-url
      (format "https://github.com/%s/%s/compare/%s...%s:%s"
              (car base-info) (cdr base-info) *pr-base-branch* (car branch-info) branch))))
 
-(defun fjl/get-github-info (branch)
+(defun fjl/get-github-info (branch get-remote)
   "Returns a cons containing the github username in the car and
 the remote repository name in the cdr."
-  (let ((remote (magit-get "remote" (magit-get-remote branch) "url")))
+  (let ((remote (magit-get "remote" (funcall get-remote branch) "url")))
     (when (null remote)
       (error "current branch has no remote"))
     (save-match-data
