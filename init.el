e15c351a172c26b57602d72c92d36730916ea931
Felix Lange <fjl@twurst.com>
2015-12-10 20:52:01 +0100

    speed up init

--- init.el
+++ init.el
@@ -1,10 +1,16 @@
 ;; -*- mode: Emacs-Lisp; fill-column: 78; -*-
 
+;; Added by Package.el.
+;(package-initialize)
+
 ;; (push (lambda (f) (message "loading %s" f)) after-load-functions)
 
 ;; Don't warn unless it's serious.
-(eval-when-compile (require 'warnings))
-(setq warning-minimum-level :error)
+;; (eval-when-compile (require 'warnings))
+;; (setq warning-minimum-level :error)
+
+;; Inhibit GC during initialization.
+(setq gc-cons-threshold (* 20 1024 1024))
 
 ;; Load the bootstrap file. This sets up all load paths and activates all packages.
 (let* ((init-dir (file-name-directory (file-truename (or load-file-name buffer-file-name))))
@@ -49,6 +55,9 @@
   (autoload 'mu4e-user-agent "mu4e" nil nil)
   (eval-after-load 'mu4e '(require 'init-mu4e))
   (setq mail-user-agent 'mu4e-user-agent))
+
+;; Set GC trigger back to a reasonable value after initializing everything.
+(setq gc-cons-threshold (* 1 1024 1024))
 
 ;; Enable some disabled commands. This goes last because emacs adds
 ;; them to the end of this file when enabling interactively.
