af75c249dba2193b0113a508f5039d9f067a5f42
Felix Lange <fjl@twurst.com>
2015-12-10 20:52:25 +0100

    add darkroom

---
+++ elpa/darkroom-0.1/darkroom-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darkroom" "0.1" "Remove visual distractions and focus on writing" '((cl-lib "0.5")) :url "http://elpa.gnu.org/packages/darkroom.html" :keywords '("convenience" "emulations"))
