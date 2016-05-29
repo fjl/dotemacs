2e73c90a7a5dc6e3be58012980bd8217c9885023
Felix Lange <fjl@twurst.com>
2016-05-29 02:25:04 +0200

    add ivy-hydra, weechat

---
+++ elpa/tracking-20151129.319/tracking-pkg.el
@@ -0,0 +1,4 @@
+(define-package "tracking" "20151129.319" "Buffer modification tracking" 'nil :url "https://github.com/jorgenschaefer/circe/wiki/Tracking")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
