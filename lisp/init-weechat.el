bc4d5cdc80bc92eebf7991eb3349949f3f469e20
Felix Lange <fjl@twurst.com>
2016-07-08 20:41:46 +0200

    disarm password callback around weechat-connect

--- lisp/init-weechat.el
+++ lisp/init-weechat.el
@@ -2,11 +2,18 @@
 
 ;;;###autoload
 (defun weechat-connect-thick ()
-  (weechat-connect "thick.fjl.io" 9000 "" "ssh -W localhost:%p %h"))
+  (interactive)
+  (with-temp-buffer
+    (setq-local weechat-password-callback (lambda () ""))
+    (weechat-connect "thick.fjl.io" 9000 "" "ssh -W localhost:%p %h")))
 
 (defun fjl/weechat-mode-setup ()
+  ;; Hack to enable reconnect.
+  (setq-local weechat-password-callback (lambda () ""))
   (setq truncate-lines nil)
-  (setq word-wrap t))
+  (setq word-wrap t)
+  (visual-line-mode 1)
+  (require 'weechat-tracking))
 
 ;;;###autoload
 (add-hook 'weechat-mode-hook 'fjl/weechat-mode-setup)
