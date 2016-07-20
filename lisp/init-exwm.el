7956e3fb501385a148ff9a38a871d5374165f566
Felix Lange <fjl@twurst.com>
2016-07-20 09:07:40 +0200

    improve exwm config

--- lisp/init-exwm.el
+++ lisp/init-exwm.el
@@ -6,6 +6,9 @@
 (setq use-dialog-box nil)
 (server-start)
 
+(setq exwm-workspace-show-all-buffers nil)
+(setq exwm-layout-show-all-buffers nil)
+
 ;; Disable auto update of keysyms to remove awkward
 ;; typing pause after pressing space.
 (setq xcb:keysyms:auto-update nil)
@@ -30,50 +33,54 @@
                       (string= "gimp" exwm-instance-name))
               (exwm-workspace-rename-buffer exwm-title))))
 
-;; We always need a way to go back to line-mode from char-mode.
-(exwm-input-set-key (kbd "C-c r") 'exwm-reset)
-;; Switch workspace interactively.
-(exwm-input-set-key (kbd "C-c v") 'exwm-workspace-switch)
-(exwm-input-set-key (kbd "C-c M-v") 'exwm-workspace-move-window)
-;; Set shortcuts to switch to a certain workspace.
-(exwm-input-set-key (kbd "s-<f1>") (lambda () (interactive) (exwm-workspace-switch 0)))
-(exwm-input-set-key (kbd "s-<f2>") (lambda () (interactive) (exwm-workspace-switch 1)))
-(exwm-input-set-key (kbd "s-<f3>") (lambda () (interactive) (exwm-workspace-switch 2)))
-(exwm-input-set-key (kbd "s-<f4>") (lambda () (interactive) (exwm-workspace-switch 3)))
-(exwm-input-set-key (kbd "s-S-<f1>") (lambda () (interactive) (exwm-workspace-move-window 0)))
-(exwm-input-set-key (kbd "s-S-<f2>") (lambda () (interactive) (exwm-workspace-move-window 1)))
-(exwm-input-set-key (kbd "s-S-<f3>") (lambda () (interactive) (exwm-workspace-move-window 2)))
-(exwm-input-set-key (kbd "s-S-<f4>") (lambda () (interactive) (exwm-workspace-move-window 3)))
-(exwm-input-set-key (kbd "C-c <f1>") (lambda () (interactive) (exwm-workspace-switch 0)))
-(exwm-input-set-key (kbd "C-c <f2>") (lambda () (interactive) (exwm-workspace-switch 1)))
-(exwm-input-set-key (kbd "C-c <f3>") (lambda () (interactive) (exwm-workspace-switch 2)))
-(exwm-input-set-key (kbd "C-c <f4>") (lambda () (interactive) (exwm-workspace-switch 3)))
-(exwm-input-set-key (kbd "C-c 1") 'launcher)
-(exwm-input-set-key (kbd "s-1") 'launcher)
-
+;; Set up emacs emulation bindings for line mode.
 (exwm-input-set-simulation-keys
- '(([?\C-b] . left)
+ `(([?\C-b] . left)
    ([?\C-f] . right)
    ([?\C-p] . up)
    ([?\C-n] . down)
-   ([?\C-a] . home)
-   ([?\C-e] . end)
-   ([?\M-v] . prior)
-   ([?\C-v] . next)))
+   ([?\M-<] . home)
+   ([?\M->] . end)
+   ([?\M-w] . ?\C-c)
+   ([?\C-y] . ?\C-v)))
 
-(defun sh! (command)
+(defun fjl/shell-lambda (command)
   (lambda ()
     (interactive)
     (shell-command command)))
-(progn
-  (exwm-input-set-key (kbd "<XF86MonBrightnessDown>") (sh! "brightness down"))
-  (exwm-input-set-key (kbd "<XF86MonBrightnessUp>")   (sh! "brightness up"))
-  (exwm-input-set-key (kbd "<XF86AudioRaiseVolume>")  (sh! "amixer set Master 8%+"))
-  (exwm-input-set-key (kbd "<XF86AudioLowerVolume>")  (sh! "amixer set Master 8%-"))
-  (exwm-input-set-key (kbd "<XF86AudioMute>")         (sh! "amixer set Master 0"))
-  (exwm-input-set-key (kbd "s-l")                     (sh! "loginctl lock-session")))
 
-;; Flush key bindings. This should happen by default, but doesn't.
-(exwm-input--update-global-prefix-keys)
+;; Set up global bindings.
+(progn
+  (exwm-input-set-key (kbd "C-c r") 'exwm-reset)
+  (exwm-input-set-key (kbd "s-r") 'exwm-reset)
+  (exwm-input-set-key (kbd "<C-escape>") 'exwm-reset)
+  ;; Switch workspace interactively.
+  (exwm-input-set-key (kbd "C-c v") 'exwm-workspace-switch)
+  (exwm-input-set-key (kbd "C-c M-v") 'exwm-workspace-move-window)
+  ;; Set shortcuts to switch to a certain workspace.
+  (exwm-input-set-key (kbd "s-<f1>") (lambda () (interactive) (exwm-workspace-switch 0)))
+  (exwm-input-set-key (kbd "s-<f2>") (lambda () (interactive) (exwm-workspace-switch 1)))
+  (exwm-input-set-key (kbd "s-<f3>") (lambda () (interactive) (exwm-workspace-switch 2)))
+  (exwm-input-set-key (kbd "s-<f4>") (lambda () (interactive) (exwm-workspace-switch 3)))
+  (exwm-input-set-key (kbd "s-S-<f1>") (lambda () (interactive) (exwm-workspace-move-window 0)))
+  (exwm-input-set-key (kbd "s-S-<f2>") (lambda () (interactive) (exwm-workspace-move-window 1)))
+  (exwm-input-set-key (kbd "s-S-<f3>") (lambda () (interactive) (exwm-workspace-move-window 2)))
+  (exwm-input-set-key (kbd "s-S-<f4>") (lambda () (interactive) (exwm-workspace-move-window 3)))
+  (exwm-input-set-key (kbd "C-c <f1>") (lambda () (interactive) (exwm-workspace-switch 0)))
+  (exwm-input-set-key (kbd "C-c <f2>") (lambda () (interactive) (exwm-workspace-switch 1)))
+  (exwm-input-set-key (kbd "C-c <f3>") (lambda () (interactive) (exwm-workspace-switch 2)))
+  (exwm-input-set-key (kbd "C-c <f4>") (lambda () (interactive) (exwm-workspace-switch 3)))
+  ;; Launching programs.
+  (exwm-input-set-key (kbd "s-1") 'launcher)
+  ;; Brightness/audio.
+  (exwm-input-set-key (kbd "<XF86MonBrightnessDown>") (fjl/shell-lambda "brightness down"))
+  (exwm-input-set-key (kbd "<XF86MonBrightnessUp>")   (fjl/shell-lambda "brightness up"))
+  (exwm-input-set-key (kbd "<XF86AudioRaiseVolume>")  (fjl/shell-lambda (concat user-emacs-directory "pa-vol.sh plus")))
+  (exwm-input-set-key (kbd "<XF86AudioLowerVolume>")  (fjl/shell-lambda (concat user-emacs-directory "pa-vol.sh minus")))
+  (exwm-input-set-key (kbd "<XF86AudioMute>")         (fjl/shell-lambda (concat user-emacs-directory "pa-vol.sh mute")))
+  ;; Screen Lock
+  (exwm-input-set-key (kbd "s-l") (fjl/shell-lambda "loginctl lock-session"))
+  ;; Flush key bindings. This should happen by default, but doesn't.
+  (exwm-input--update-global-prefix-keys))
 
 (provide 'init-exwm)
---
+++ pa-vol.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# from https://github.com/boredomwontgetus/pa-vol
+#
+# CHANGES, RE-VIEWS/WRITES, IMPROVEMENTS:
+# mhasko: functions; VOL-steps;
+# gashapon: autodectect default sink
+# konrad: more reliable way to autodetect default sink
+
+# get default sink name
+SINK_NAME=$(pacmd dump | perl -a -n -e 'print $F[1] if /set-default-sink/')
+# try this line instead of the one above if you got problems with the detection of your default sink.
+#SINK_NAME=$(pactl stat | grep "alsa_output" | perl -a -n -e 'print $F[2]')
+
+# set max allowed volume; 0x10000 = 100%
+VOL_MAX="0x10000"
+
+STEPS="16" # 2^n
+VOL_STEP=$((VOL_MAX / STEPS))
+
+VOL_NOW=`pacmd dump | grep -P "^set-sink-volume $SINK_NAME\s+" | perl -p -n -e 's/.+\s(.x.+)$/$1/'`
+MUTE_STATE=`pacmd dump | grep -P "^set-sink-mute $SINK_NAME\s+" | perl -p -n -e 's/.+\s(yes|no)$/$1/'`
+
+function plus() {
+	VOL_NEW=$((VOL_NOW + VOL_STEP))
+	if [ $VOL_NEW -gt $((VOL_MAX)) ]; then
+		VOL_NEW=$((VOL_MAX))
+	fi
+	pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
+	VOL_NOW=$VOL_NEW
+}
+
+function minus() {
+	VOL_NEW=$((VOL_NOW - VOL_STEP))
+	if [ $(($VOL_NEW)) -lt $((0x00000)) ]; then
+		VOL_NEW=$((0x00000))
+	fi
+	pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
+	VOL_NOW=$VOL_NEW
+}
+
+function mute() {
+	if [ $MUTE_STATE = no ]; then
+		pactl set-sink-mute $SINK_NAME 1
+		MUTE_STATE=yes
+	elif [ $MUTE_STATE = yes ]; then
+		pactl set-sink-mute $SINK_NAME 0
+		MUTE_STATE=no
+	fi
+}
+
+function get() {
+	BAR=""
+	if [ $MUTE_STATE = yes ]; then
+		BAR="mute"
+		ITERATOR=$((STEPS / 2 - 2))
+		while [ $ITERATOR -gt 0 ]; do
+			BAR=" ${BAR} "
+			ITERATOR=$((ITERATOR - 1))
+		done
+	else
+		DENOMINATOR=$((VOL_MAX / STEPS))
+		LINES=$((VOL_NOW / DENOMINATOR))
+		DOTS=$((STEPS - LINES))
+		while [ $LINES -gt 0 ]; do
+			BAR="${BAR}|"
+			LINES=$((LINES - 1))
+		done
+		while [ $DOTS -gt 0 ]; do
+			BAR="${BAR}."
+			DOTS=$((DOTS - 1))
+		done
+	fi
+	echo "Audio Volume: $BAR"
+}
+
+case "$1" in
+	plus)  plus;  get ;;
+	minus) minus; get ;;
+	mute)  mute;  get ;;
+	get)   get
+esac
