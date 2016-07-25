9d6e3415b9e5df73df7f2d74a78b19d25c989e43
Felix Lange <fjl@twurst.com>
2016-07-25 15:12:52 +0200

    add missing git fetch to update-exwm.sh

--- scripts/update-exwm.sh
+++ scripts/update-exwm.sh
@@ -3,8 +3,10 @@
 set -e
 
 function update-subtree () {
-    if ! git remote get-url $1-upstream 2>/dev/null; then
+    if ! git remote get-url $1-upstream >/dev/null; then
         git remote add -f $1-upstream https://github.com/ch11ng/$1
+    else
+        git fetch -v $1-upstream
     fi
     [ -d elpa/$1 ] && git rm -qr elpa/$1
     git read-tree --prefix=elpa/$1/ -u $1-upstream/master
