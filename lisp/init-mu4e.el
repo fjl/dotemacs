04f1079eecc6af6acd92a1c2461c93caf36deb95
Felix Lange <fjl@twurst.com>
2016-08-09 01:34:19 +0200

    improve mu4e setup

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -1,50 +1,66 @@
 (require 'mu4e)
-(require 'mu4e-multi)
 (require 's)
 (require 'cl-lib)
 (require 'init-commands)
 
-;;; Multi-account setup
-
-(setq mu4e-multi-account-alist
-      '(("twurst.com"
-         (mu4e-inbox-folder   . "/twurst.com/INBOX")
-         (mu4e-archive-folder . "/twurst.com/archive")
-         (mu4e-sent-folder    . "/twurst.com/Sent Messages")
-         (mu4e-drafts-folder  . "/twurst.com/Drafts")
-         (mu4e-trash-folder   . "/twurst.com/Deleted Messages")
-         (mu4e-junk-folder    . "/twurst.com/Junk")
-         (user-mail-address   . "fjl@twurst.com"))
-        ("ethdev.com"
-         (mu4e-inbox-folder   . "/ethdev.com/Inbox")
-         (mu4e-archive-folder . "/ethdev.com/[Gmail]/Archive")
-         (mu4e-sent-folder    . "/ethdev.com/[Gmail]/Sent Mail")
-         (mu4e-trash-folder   . "/ethdev.com/[Gmail]/Trash")
-         (mu4e-drafts-folder  . "/ethdev.com/[Gmail]/Drafts")
-         (mu4e-junk-folder    . "/ethdev.com/[Gmail]/Spam")
-         (user-mail-address   . "felix@ethdev.com"))))
-
-(setq mu4e-multi-default-account "twurst.com")
-
-(mu4e-multi-enable)
+(setq mu4e-contexts
+      (list
+       (make-mu4e-context
+        :name "Private"
+        :match-func (lambda (msg)
+                      (when msg
+                        (or
+                         (s-prefix-p "/twurst.com" (mu4e-message-field msg :maildir))
+                         (mu4e-message-contact-field-matches msg :to "fjl@twurst.com")
+                         (mu4e-message-contact-field-matches msg :to "br@twurst.com"))))
+        :vars '((user-mail-address   . "fjl@twurst.com")
+                (user-full-name      . "Felix Lange")
+                (mu4e-inbox-folder   . "/twurst.com/INBOX")
+                (mu4e-refile-folder  . "/twurst.com/archive")
+                (mu4e-sent-folder    . "/twurst.com/Sent Messages")
+                (mu4e-drafts-folder  . "/twurst.com/Drafts")
+                (mu4e-trash-folder   . "/twurst.com/Deleted Messages")
+                (mu4e-junk-folder    . "/twurst.com/Junk")))
+       (make-mu4e-context
+        :name "Ethereum"
+        :match-func (lambda (msg)
+                      (when msg
+                        (or
+                         (s-prefix-p "/ethereum.org" (mu4e-message-field msg :maildir))
+                         (mu4e-message-contact-field-matches msg :to "felix@ethdev.com")
+                         (mu4e-message-contact-field-matches msg :to "fjl@ethereum.org"))))
+        :vars '((user-mail-address   . "fjl@ethereum.org")
+                (user-full-name      . "Felix Lange")
+                (mu4e-inbox-folder   . "/ethereum.org/INBOX")
+                (mu4e-refile-folder  . "/ethereum.org/[Gmail].All Mail")
+                (mu4e-sent-folder    . "/ethereum.org/[Gmail].Sent Mail")
+                (mu4e-trash-folder   . "/ethereum.org/[Gmail].Trash")
+                (mu4e-drafts-folder  . "/ethereum.org/[Gmail].Drafts")
+                (mu4e-junk-folder    . "/ethereum.org/[Gmail].Spam")))))
 
 (defun fjl/mu4e-expand-mailbox-alt (var)
   (let ((folders
          (cl-reduce (lambda (folders account)
-                      (let ((b (assoc var (cdr account))))
+                      (let ((b (assoc var (mu4e-context-vars account))))
                         (if (cdr b)
                             (cons (concat "maildir:\"" (cdr b) "\"") folders)
                           folders)))
-                    mu4e-multi-account-alist :initial-value nil)))
+                    mu4e-contexts :initial-value nil)))
     (concat "(" (s-join " OR " folders) ")")))
 
 (defun fjl/mu4e-expand-bookmark (bm)
   "Expands <in x> in mu4e bookmark definitions."
-  (cons (replace-regexp-in-string "<in \\([^>]*\\)>"
-                                  (lambda (s)
-                                    (fjl/mu4e-expand-mailbox-alt (intern (match-string 1 s))))
-                                  (car bm))
-        (cdr bm)))
+  (replace-regexp-in-string
+   "<in \\([^>]*\\)>"
+   (lambda (s) (fjl/mu4e-expand-mailbox-alt (intern (match-string 1 s))))
+   bm))
+
+(defun fjl/mu4e-sent-folder ()
+  (if (string= (message-sendmail-envelope-from) "fjl@ethereum.org")
+      'delete ;; for Gmail
+    'sent))
+
+(setq mu4e-sent-messages-behavior #'fjl/mu4e-sent-folder)
 
 ;; Message view setup
 
@@ -55,6 +71,9 @@
 (add-hook 'mu4e-view-mode-hook 'fjl/mu4e-view-mode-hook)
 
 ;; Commands and Bindings
+
+(define-key mu4e-headers-mode-map (kbd "C-c C-u") 'mu4e-update-index)
+(define-key mu4e-view-mode-map (kbd "C-c C-u") 'mu4e-update-index)
 
 ;; Patch the trash mark action to not mark the message as trashed.
 ;; mbsync interprets 'trashed' as 'deleted', so the messages are
@@ -71,43 +90,54 @@
                 (if (eq (car m) 'trash) fjl/mu4e-trash-mark m))
               mu4e-marks))
 
-(defun fjl/render-html-mail ()
-  "Render HTML mail with shr."
-  (require 'shr)
-  (let ((dom (libxml-parse-html-region (point-min) (point-max)))
-        ;; Disable image loading.
-        (shr-inhibit-images t)
-        ;; Tell shr to fit into the wrap width.
-        (shr-width 99))
-    (erase-buffer)
-    (shr-insert-document dom)
-    (goto-char (point-min))))
-
 ;; Misc Settings
 
-(setq mu4e-attachment-dir "/Users/fjl/Downloads"
-      mu4e-use-fancy-chars nil
-      mu4e-view-show-images t
-      mu4e-view-show-addresses t
+(setq mu4e-attachment-dir "~/Downloads/"
       mu4e-change-filenames-when-moving t ;; for mbsync
       mu4e-completing-read-function 'ivy-completing-read
+      mu4e-compose-context-policy 'ask-if-none
+      mu4e-compose-format-flowed t
       mu4e-compose-signature nil
-      mu4e-headers-skip-duplicates t
-      mu4e-headers-include-related t
       mu4e-compose-signature-auto-include nil
-      mu4e-html2text-command 'fjl/render-html-mail ;; or "w3m -T text/html -S -O utf8 -dump -cols 90"
-      mu4e-get-mail-command "mbsync -aVX | cat" ;; | cat to get rid of tty progress indicator
+      mu4e-context-policy 'pick-first
+      mu4e-headers-include-related t
+      mu4e-headers-skip-duplicates t
       mu4e-maildir "~/Mail"
-      mu4e-user-mailing-lists '(("go-ethereum.ethereum.github.com" . "geth")))
+      mu4e-use-fancy-chars nil
+      mu4e-user-mail-address-list '("fjl@twurst.com" "br@twurst.com" "felix@ethdev.com" "fjl@ethereum.org")
+      mu4e-user-mailing-lists '(("go-ethereum.ethereum.github.com" . "geth"))
+      mu4e-view-show-addresses t
+      mu4e-view-show-images t)
 
 (setq mu4e-bookmarks
-      (mapcar 'fjl/mu4e-expand-bookmark
-              '(("flag:unread AND NOT <in mu4e-trash-folder>" "Unread messages" ?u)
-                ("date:today..now AND NOT <in mu4e-trash-folder>" "Today's messages" ?t)
-                ("date:7d..now AND NOT <in mu4e-trash-folder>" "Last 7 days" ?w)
-                ("mime:image/*" "Messages with images" ?p)
-                ("<in mu4e-inbox-folder>" "INBOX" ?I)
-                ("<in mu4e-archive-folder>" "Archive" ?a)
-                ("<in mu4e-inbox-folder> AND NOT flag:list" "INBOX (no list mail)" ?i))))
+      (list
+       (make-mu4e-bookmark
+        :name  "Unread"
+        :query '(fjl/mu4e-expand-bookmark "flag:unread AND NOT <in mu4e-trash-folder>")
+        :key ?U)
+       (make-mu4e-bookmark
+        :name  "Unread (nolist)"
+        :query '(fjl/mu4e-expand-bookmark "flag:unread AND NOT <in mu4e-trash-folder> AND NOT flag:list")
+        :key ?u)
+       (make-mu4e-bookmark
+        :name "Today's messages"
+        :query '(fjl/mu4e-expand-bookmark "date:today..now AND NOT <in mu4e-trash-folder>")
+        :key ?t)
+       (make-mu4e-bookmark
+        :name "Last 7 days"
+        :query '(fjl/mu4e-expand-bookmark "date:7d..now AND NOT <in mu4e-trash-folder>")
+        :key ?w)
+       (make-mu4e-bookmark
+        :name "INBOX"
+        :query '(fjl/mu4e-expand-bookmark "<in mu4e-inbox-folder>")
+        :key ?I)
+       (make-mu4e-bookmark
+        :name "INBOX (nolist)"
+        :query '(fjl/mu4e-expand-bookmark "<in mu4e-inbox-folder> AND NOT flag:list")
+        :key ?i)
+       (make-mu4e-bookmark
+        :name "Archive"
+        :query '(fjl/mu4e-expand-bookmark "<in mu4e-refile-folder>")
+        :key ?a)))
 
 (provide 'init-mu4e)
