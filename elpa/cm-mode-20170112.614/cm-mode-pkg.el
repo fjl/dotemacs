ba5a1b6d789c0744539bb106b5b593253c8b32ed
Felix Lange <fjl@twurst.com>
2017-03-14 13:23:56 +0100

    upgrade packages

--- elpa/cm-mode-20170112.614/cm-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cm-mode" "20170112.614" "Minor mode for CriticMarkup" '((cl-lib "0.5")) :keywords '("text" "markdown"))
---
+++ elpa/cm-mode-20170203.1307/cm-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cm-mode" "20170203.1307" "Minor mode for CriticMarkup" '((cl-lib "0.5")) :keywords '("text" "markdown"))
