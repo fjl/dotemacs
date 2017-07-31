6b4932eae6402d384b996a459eec34576352f255
Felix Lange <fjl@twurst.com>
2017-07-31 11:30:58 +0200

    upgrade packages

---
+++ elpa/geiser-20170704.1044/geiser-pkg.el
@@ -0,0 +1,4 @@
+(define-package "geiser" "20170704.1044" "GNU Emacs and Scheme talk to each other" 'nil :url "http://www.nongnu.org/geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/solidity-mode-20161017.1506/solidity-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "solidity-mode" "20161017.1506" "Major mode for ethereum's solidity language" 'nil :keywords '("languages"))
