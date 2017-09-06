afcdb8530c188af76590cebbe873b0dc97fe2ccb
Felix Lange <fjl@twurst.com>
2017-09-06 23:26:05 +0200

    upgrade packages

--- elpa/dtrt-indent-20160620.329/dtrt-indent-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "dtrt-indent" "20160620.329" "Adapt to foreign indentation offsets" 'nil :keywords
-  '("convenience" "files" "languages" "c"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
