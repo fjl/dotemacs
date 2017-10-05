7720452ff570ad66c1fd69dbd23748f92d3be6dc
Felix Lange <fjl@twurst.com>
2017-10-05 19:37:13 +0200

    add logview

---
+++ elpa/logview-20170928.820/logview-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "logview" "20170928.820" "Major mode for viewing log files" '((emacs "24.1") (datetime "0.3")) :commit "72b6c5349206172a146b2c730b8ac040a92ebc3f" :url "https://github.com/doublep/logview" :keywords '("files" "tools"))
