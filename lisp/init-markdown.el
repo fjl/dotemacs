b90c013a4cbf3e18e219a5c932c11bc45758b158
Felix Lange <fjl@twurst.com>
2017-10-26 22:19:34 +0200

    disable automatic variable-pitch-mode for markdown

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,8 +5,7 @@
 (defun fjl/markdown-mode-hook ()
   (setq truncate-lines nil)
   (setq word-wrap t)
-  (setq indent-tabs-mode nil)
-  (variable-pitch-mode 1))
+  (setq indent-tabs-mode nil))
 
 ;;;###autoload
 (add-hook 'markdown-mode-hook 'fjl/markdown-mode-hook)
