420065fc1981c7310912c5460fa1a0200da2d3cf
Felix Lange <fjl@twurst.com>
2017-12-05 21:14:39 +0100

    upgrade packages

--- elpa/counsel-20171017.1029/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20171017.1029" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "4a66bb6afd981923f923483168c03a9d89c61191" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
--- elpa/weechat-20161211.259/weechat-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "weechat" "20161211.259" "Chat via WeeChat's relay protocol in Emacs"
-  '((s "1.3.1")
-    (cl-lib "0.2")
-    (emacs "24")
-    (tracking "1.2")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
