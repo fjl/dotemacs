a52ada1927b88d079c594d0ac9f222f546d18c6b
Felix Lange <fjl@twurst.com>
2018-05-08 12:08:55 +0200

    delete some packages

--- elpa/logview-20180216.1445/logview-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "logview" "20180216.1445" "Major mode for viewing log files" '((emacs "24.4") (datetime "0.3")) :commit "9ec279d933923dab2d8d1f140adc43073dab6433" :url "https://github.com/doublep/logview" :keywords '("files" "tools"))
