361e6b507fd6f2a9f67847f4e94c8bdd6fb5c702
Felix Lange <fjl@twurst.com>
2019-06-03 13:44:07 +0200

    upgrade packages

---
+++ elpa/counsel-20190603.1059/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20190603.1059" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "7feb14f476e108dc10fe5380ee7c33de3b5fe3f1" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
