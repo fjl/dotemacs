647ecc8e388fc9fc462be097fe87386e05557a35
Felix Lange <fjl@twurst.com>
2019-11-18 13:26:34 +0100

    upgrade packages

--- elpa/swiper-20191021.1015/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/fjl/.emacs.d/elpa/swiper-20191021.1015/swiper.el  -*- no-byte-compile: t -*-
-(define-package "swiper" "20191021.1015" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "1bf7f91f00bd90b9ffe8e3e8228a5df43e6d5122" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
