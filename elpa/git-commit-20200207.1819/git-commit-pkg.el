a7ef0cd59adf5b8c0de1bf2ed7fa1a279c0f567e
Felix Lange <fjl@twurst.com>
2020-02-21 16:42:34 +0100

    upgrade packages and re-add org-plus-contrib

---
+++ elpa/git-commit-20200207.1819/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "b857fc1b832dfbd557ed48b3e075848579dc2c66" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
