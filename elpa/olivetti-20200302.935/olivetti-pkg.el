30b43c3d8cdd1cfaf9a9d2f1d0baf7a091225567
Felix Lange <fjl@twurst.com>
2020-03-05 17:31:11 +0100

    upgrade packages

---
+++ elpa/olivetti-20200302.935/olivetti-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "olivetti" "20200302.935" "Minor mode for a nice writing environment" '((emacs "24.5")) :commit "b5377c5bdf6a83009435e421756bfffb75714657" :keywords '("wp" "text") :authors '(("William Rankin" . "code@william.bydasein.com")) :maintainer '("William Rankin" . "code@william.bydasein.com") :url "https://gthub.com/rnkn/olivetti")
---
+++ elpa/slime-company-20200304.1107/slime-company-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime-company" "20200304.1107" "slime completion backend for company mode" '((emacs "24.4") (slime "2.13") (company "0.9.0")) :commit "e9153e42ec8f2089ea129ce24488dd3b5e0b9e47" :keywords '("convenience" "lisp" "abbrev") :authors '(("Ole Arndt" . "anwyn@sugarshark.com")) :maintainer '("Ole Arndt" . "anwyn@sugarshark.com"))
