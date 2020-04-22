9793fc2841e3473ef6bf65f8718b48d47cb24358
Felix Lange <fjl@twurst.com>
2020-04-22 13:37:11 +0200

    upgrade packages

---
+++ elpa/slime-company-20200424.1045/slime-company-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/fjl/.emacs.d/elpa/slime-company-20200424.1045/slime-company.el  -*- no-byte-compile: t -*-
+(define-package "slime-company" "20200424.1045" "slime completion backend for company mode" '((emacs "24.4") (slime "2.13") (company "0.9.0")) :commit "cec203c45ebd678b05341d1cdbe420ad07a1b8e0" :keywords '("convenience" "lisp" "abbrev") :authors '(("Ole Arndt" . "anwyn@sugarshark.com")) :maintainer '("Ole Arndt" . "anwyn@sugarshark.com"))
