82083475521073833f9ece36a2944ab3f363984b
Felix Lange <fjl@twurst.com>
2021-07-20 15:13:09 +0200

    upgrade packages

---
+++ elpa/slime-20210709.2051/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20210709.2051" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "68f5623f13c9a0d3d47ce70cf56928e00483d9d6" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/transient-20210712.1626/transient-pkg.el
@@ -0,0 +1,12 @@
+(define-package "transient" "20210712.1626" "Transient commands"
+  '((emacs "25.1"))
+  :commit "e90481000f071e9a26a1cc0f40d347f7d3e2201e" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("bindings")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
