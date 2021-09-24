e4492cc6e1bfa5efc82b0effdfd138d6b31f4b32
Felix Lange <fjl@twurst.com>
2021-09-24 14:25:08 +0200

    add speed-type

---
+++ elpa/speed-type-20191204.1107/speed-type-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from speed-type.el  -*- no-byte-compile: t -*-
+(define-package "speed-type" "20191204.1107" "Practice touch and speed typing" '((emacs "24.3") (cl-lib "0.3")) :commit "5ef695f7159aa1f20c7c9e55f0c39bcdacce8d21" :authors '(("Gunther Hagleitner")) :maintainer '("Julien Pagès" . "j.parkouss@gmail.com") :keywords '("games") :url "https://github.com/parkouss/speed-type")
