67b9ef36bc4ed0b4faef90075cc33b2aa6aaecb0
Felix Lange <fjl@twurst.com>
2021-12-14 16:29:34 +0100

    upgrade & cleanup installed packages
    - remove test version of org
    - uninstall geiser

---
+++ elpa/erlang-20211213.1046/erlang-pkg.el
@@ -0,0 +1,11 @@
+(define-package "erlang" "20211213.1046" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "9b07aedd669c2cb1e12e8a9b4f06210d0892041e" :authors
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("erlang" "languages" "processes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20211214.1007/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20211214.1007" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "6724f128471dad1bcdd383937e2d1ed79488ec00" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
