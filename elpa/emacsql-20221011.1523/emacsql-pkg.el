d894923a1260af415747412589e0b698d98c322e
Felix Lange <fjl@twurst.com>
2022-10-13 11:53:31 +0200

    upgrade packages

---
+++ elpa/emacsql-20221011.1523/emacsql-pkg.el
@@ -0,0 +1,10 @@
+(define-package "emacsql" "20221011.1523" "High-level SQL database front-end"
+  '((emacs "25.1"))
+  :commit "bfbd0c3a73f53d44d522995d065bd930f00c9316" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :url "https://github.com/magit/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20221010.2033/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20221010.2033" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "759fb79b4fc62b9b8d2dcd8a4e4db951af090fdb" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
