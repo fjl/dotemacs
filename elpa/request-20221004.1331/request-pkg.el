c56d157df00cc46d245fe4b8d1b79702aab2507e
Felix Lange <fjl@twurst.com>
2022-10-21 17:22:01 +0200

    add package 'ein'

---
+++ elpa/request-20221004.1331/request-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from request.el  -*- no-byte-compile: t -*-
+(define-package "request" "20221004.1331" "Compatible layer for URL request" '((emacs "24.4")) :commit "4dad57086b1b82ba16533a7ab4ed64fbd03603ba" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
