d0a0f326b1f2a83b77189de59a11b00f86c4b9e8
Felix Lange <fjl@twurst.com>
2023-03-01 19:26:04 +0100

    upgrade packages and remove forge, eglot

---
+++ elpa/erlang-20230215.909/erlang-pkg.el
@@ -0,0 +1,11 @@
+(define-package "erlang" "20230215.909" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "127026003180a834e9fa5d5919c824a184faeb92" :authors
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("erlang" "languages" "processes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/slime-20230222.1526/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20230222.1526" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "e193bc5f3431a2f71f1d7a0e3f28e6dc4dd5de2d" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
