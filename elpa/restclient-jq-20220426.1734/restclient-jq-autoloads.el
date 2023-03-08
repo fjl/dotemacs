ec4b0f105ba97d4e733a34044a6b0b8e0dd3e2da
Felix Lange <fjl@twurst.com>
2023-03-08 19:36:53 +0100

    add restclient-jq and wgrep-ag

---
+++ elpa/restclient-jq-20220426.1734/restclient-jq-autoloads.el
@@ -0,0 +1,22 @@
+;;; restclient-jq-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "restclient-jq" "restclient-jq.el" (0 0 0 0))
+;;; Generated autoloads from restclient-jq.el
+
+(register-definition-prefixes "restclient-jq" '("restclient-jq-"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; restclient-jq-autoloads.el ends here
---
+++ elpa/restclient-jq-20220426.1734/restclient-jq-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from restclient-jq.el  -*- no-byte-compile: t -*-
+(define-package "restclient-jq" "20220426.1734" "Support for setting restclient vars from jq expressions" '((restclient "20200502.831") (jq-mode "0.4.1") (emacs "24.4")) :commit "ae79e7dd283890072da69b8f48aeec1afd0d9442" :authors '(("Cameron Dorrat" . "cdorrat@gmail.com")) :maintainer '("Cameron Dorrat" . "cdorrat@gmail.com") :keywords '("tools" "comm" "http" "jq") :url "https://github.com/pashky/restclient.el")
