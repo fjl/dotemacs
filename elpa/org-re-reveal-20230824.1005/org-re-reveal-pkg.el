208c30f9acf339a21af4b0b0012d649ec7e1b495
Felix Lange <fjl@twurst.com>
2023-10-31 10:11:39 +0100

    add org-re-reveal

---
+++ elpa/org-re-reveal-20230824.1005/org-re-reveal-pkg.el
@@ -0,0 +1,10 @@
+(define-package "org-re-reveal" "20230824.1005" "Org export to reveal.js presentations"
+  '((emacs "24.4")
+    (org "8.3")
+    (htmlize "1.34"))
+  :commit "d12b4acd6e1d9b1bec7e253bc417bcc830ed9da1" :keywords
+  '("tools" "outlines" "hypermedia" "slideshow" "presentation" "oer")
+  :url "https://gitlab.com/oer/org-re-reveal")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
