dafabb8ce4f4e5094c639e0f15ec514f7f32c973
Felix Lange <fjl@twurst.com>
2025-06-15 19:38:23 +0200

    advise ffap to go to line number

--- lisp/init-commands.el
+++ lisp/init-commands.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'init-bootstrap)
+(require 'dired)
 
 (defun init-compile ()
   "Recompile initialization files."
@@ -285,3 +286,35 @@ Optional INITIAL-INPUT is the initial input in the minibuffer."
     (setq fjl/dired-afplay-process (start-process "*dired afplay*" nil "afplay" file))))
 
 (provide 'init-commands)
+
+;; ffap line number extension
+;; from https://www.emacswiki.org/emacs/FindFileAtPoint
+
+(defvar ffap-file-at-point-line-number nil
+  "Variable to hold line number from the last `ffap-file-at-point' call.")
+
+(defadvice ffap-file-at-point (after ffap-store-line-number activate)
+  "Search `ffap-string-at-point' for a line number pattern and
+save it in `ffap-file-at-point-line-number' variable."
+  (let* ((string (ffap-string-at-point)) ;; string/name definition copied from `ffap-string-at-point'
+         (name
+          (or (condition-case nil
+                  (and (not (string-match "//" string)) ; foo.com://bar
+                       (substitute-in-file-name string))
+                (error nil))
+              string))
+         (line-number-string
+          (and (string-match ":[0-9]+" name)
+               (substring name (1+ (match-beginning 0)) (match-end 0))))
+         (line-number
+          (and line-number-string
+               (string-to-number line-number-string))))
+    (if (and line-number (> line-number 0))
+        (setq ffap-file-at-point-line-number line-number)
+      (setq ffap-file-at-point-line-number nil))))
+
+(defadvice find-file-at-point (after ffap-goto-line-number activate)
+  "If `ffap-file-at-point-line-number' is non-nil goto this line."
+  (when ffap-file-at-point-line-number
+    (goto-line ffap-file-at-point-line-number)
+    (setq ffap-file-at-point-line-number nil)))
