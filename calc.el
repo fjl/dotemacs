7bfb06a74ea38ff0e926f745b5655038f3385335
Felix Lange <fjl@twurst.com>
2026-03-09 20:00:27 +0100

    add calc.el

---
+++ calc.el
@@ -0,0 +1,8 @@
+;;; -*- mode: emacs-lisp; lexical-binding:t -*-
+
+;;; Mode settings stored by Calc on Mon Mar  9 18:16:05 2026
+(setq calc-display-trail nil)
+(setq calc-language 'c)
+(setq calc-vector-brackets "{}")
+;;; End of mode settings
+
